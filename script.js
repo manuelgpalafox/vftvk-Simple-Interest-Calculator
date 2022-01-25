c490d27d276547c72ffad4215393e90aff29ba14
manuelgpalafox <manuelgpalafox@hotmail.com>
2022-01-25 20:03:21 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ function compute(){
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit <strong> "+principal+", </strong> <br> at an interest rate of <strong> "+rate+"% </strong> <br> You will receive an amount of <strong>"+interest+"</strong>, <br>in the year <strong> "+year+" </strong> <br>"   
+    document.getElementById("result").innerHTML="If you deposit <strong> "+principal+" </strong> <br> at an interest rate of <strong> "+rate+"% </strong> <br> You will receive an amount of <strong>"+interest+"</strong> <br>in the year <strong> "+year+" </strong> <br>"
 }
 
 function updateRate(){
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-        
+
+function checkdata(){
+    //Create references to the input elements we wish to validate
+    var principal_number = document.getElementById("principal");
+
+    //Check if username field is empty
+    if(principal_number.value == 0){
+          alert("Please enter a positive number");
+          principal_number.focus();
+          return false;
+    }
+
+    if(principal_number.value < 0){
+        alert("Please enter a positive number");
+        principal_number.focus();
+        return false;
+  }
+}
